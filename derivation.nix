50abc77ac745102ba21abefb7b7363d327b62905
Jason Felice <jason.m.felice@gmail.com>
2024-01-09 11:45:18 -0500

    Bump flakes

--- derivation.nix
+++ derivation.nix
@@ -6,6 +6,6 @@ buildGo119Module {
   pname = "plumber-pluggo";
   version = "0.1.0";
   src = ./.;
-  vendorSha256 = "a2nddJIM5Ui1maLz2hZUssksXmvI+VQyqc5aMfZ1TbY=";
+  vendorHash = "sha256:a2nddJIM5Ui1maLz2hZUssksXmvI+VQyqc5aMfZ1TbY=";
 }
 
